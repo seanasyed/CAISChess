4ae4269b6de86064e8f5ecb1d28adf587fafc5dd
Sean Syed <seansyed@usc.edu>
2019-02-28 22:22:23 -0800

    added first two layers

--- NeuralNet.py
+++ NeuralNet.py
@@ -1,11 +1,13 @@
 import tensorflow as tf
-from tensorflow.layers import Conv2D
 
 class NeuralNet(): 
 
-	#TODO 
-
 	def __init__(self, inputData): 
-		self.inputLayer = tf.reshape()
+		self.inputLayer = tf.reshape(inputData) #TODO create input layer to transform info from graph
 	
-	def
+	def rlModel(self, labels, mode):
+		conv1 = tf.layers.conv2d(
+			inputs=self.inputLayer,
+			filters=256,
+			kernel_size=[3,3],
+			stride=1)
