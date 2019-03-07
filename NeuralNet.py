8da32b1c7fc46c86d33d79cac599570e97424718
Sean Syed <seansyed@usc.edu>
2019-03-06 21:20:38 -0800

    Add residual layers'

--- NeuralNet.py
+++ NeuralNet.py
@@ -4,6 +4,43 @@ class NeuralNet():
 
 	def __init__(self, inputData): 
 		self.inputLayer = inputData # TODO create input layer to transform info from graph
+
+	# Either 19 or 39 residual blocks. We can probably mess with that number
+	# Each residual block applies the following modules sequentially to its input
+	def residualBlock(self, input, numBlocks): # Residual blocks that add input back at the end via skip layer implementation
+
+		conv2 = tf.layers.conv2d(
+			inputs=input,
+			filters=256,
+			kernel_size=[3,3],
+			stride=1)
+
+		batchNorm2 = tf.layers.batch_normalization(conv2)
+
+		relu2 = tf.nn.relu(batchNorm2)
+
+		conv3 = tf.layers.conv2d(
+			inputs=relu2,
+			filters=256,
+			kernel_size=[3,3],
+			stride=1)
+
+		batchNorm3 = tf.layers.batch_normalization(conv3)
+
+		batchNorm3 += numBlocks
+
+		relu3 = tf.nn.relu(batchNorm3)
+
+		if numBlocks == 1: 
+			return relu3
+		else:
+			return residualBlock(relu3, numBlocks - 1)
+
+	def policyHead(self): 
+		pass
+
+	def valueHead(self): 
+		pass
 	
 	def combinationalBlock(self, labels, mode): # Beginning of the net that will invoke the residual blocks
 
@@ -18,12 +55,13 @@ class NeuralNet():
 
 		relu1 = tf.nn.relu(batchNorm1)
 
-		#TODO Invoke residual blocks
+		residual = residualBlock(relu1, 13)
 
-	# Either 19 or 39 residual blocks. We can probably mess with that number
-	# Each residual block applies the following modules sequentially to its input
-	def residualBlock(self, input): # TODO Residual blocks that add input back at the end via skip layer implementation
-		pass
+	
+
+
+
+
 
 
 
