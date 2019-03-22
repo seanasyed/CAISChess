0196fc447a9b04a03d53104c46ec3857debe3136
Sean Syed <seansyed@usc.edu>
2019-03-21 19:26:37 -0700

    finished net

--- NeuralNet.py
+++ NeuralNet.py
@@ -7,10 +7,10 @@ class NeuralNet():
 
 	# Either 19 or 39 residual blocks. We can probably mess with that number
 	# Each residual block applies the following modules sequentially to its input
-	def residualBlock(self, input, numBlocks): # Residual blocks that add input back at the end via skip layer implementation
+	def residualBlock(self, inputData): # Residual blocks that add input back at the end via skip layer implementation
 
 		conv2 = tf.layers.conv2d(
-			inputs=input,
+			inputs=inputData,
 			filters=256,
 			kernel_size=[3,3],
 			stride=1)
@@ -27,18 +27,15 @@ class NeuralNet():
 
 		batchNorm3 = tf.layers.batch_normalization(conv3)
 
-		batchNorm3 += numBlocks
+		batchNorm3 += inputData
 
 		relu3 = tf.nn.relu(batchNorm3)
 
-		if numBlocks == 1: 
-			return relu3
-		else:
-			return residualBlock(relu3, numBlocks - 1)
+		return relu3
 
-	def policyHead(self, input): 
+	def policyHead(self, inputData): 
 		conv4 = tf.layers.conv2d(
-			inputs=input,
+			inputs=inputData,
 			filters=2,
 			kernel_size=[1,1],
 			stride=1)
@@ -47,12 +44,35 @@ class NeuralNet():
 
 		relu4 = tf.nn.relu(batchNorm4)
 
-		# TODO return fully connected layer
+		# return fully connected layer
+		return tf.contrib.layers.fully_connected(
+			inputs=relu4, num_outputs=392)
 
 	def valueHead(self, input): 
-		pass
+		conv5 = tf.layers.conv2d(
+			inputs=input, 
+			kernel_size=[1,1], 
+			filters=1, 
+			stride=1)
+
+		batchNorm5 = tf.layers.batch_normalization(conv5)
+
+		relu5 = tf.nn.relu(batchNorm5)
+
+		fullyConnected = tf.contrib.layers.fully_connected(
+			inputs=relu5, num_outputs=256)
+
+		relu6 = tf.nn.relu(fullyConnected)
+
+		fullyConnected = tf.contrib.layers.fully_connected(
+			inputs=relu6, num_outputs=1)
+
+		return tf.math.tanh(fullyConnected)
+
+
+
 	
-	def combinationalBlock(self, labels, mode): # Beginning of the net that will invoke the residual blocks
+	def convolutionalBlock(self): # Beginning of the net that will invoke the residual blocks
 
 		# Convolutional Block
 		conv1 = tf.layers.conv2d(
@@ -65,15 +85,18 @@ class NeuralNet():
 
 		relu1 = tf.nn.relu(batchNorm1)
 
-		residual = residualBlock(relu1, 13)
+		return relu1
 
-		policy = policyHead(residual)
+	def initNeuralNet(self, numResidualBlocks): 
+		self.numResidualBlocks = numResidualBlocks
 
-		value = valueHead(residual)
+		convolutionalBlock = convolutionalBlock()
 
-	
+		for n in range (10): 
+			residualBlock = residualBlock(convolutionalBlock)
 
-
+		policy = policyHead(residualBlock)
+		value = valueHead(residualBlock)
 
 
 
